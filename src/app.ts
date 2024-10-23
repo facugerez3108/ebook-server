1d5e23ae052e5e4be5ce0a7c698050622065cb01
unknown <facuugerez37@gmail.com>
2024-10-23 19:32:14 -0300

    config app.ts in cors

--- src/app.ts
+++ src/app.ts
@@ -20,7 +20,9 @@ if (config.env !== 'test') {
 }
 
 // set security HTTP headers
-app.use(helmet());
+app.use(helmet({
+  crossOriginResourcePolicy: false,
+}));
 
 // parse json request body
 app.use(express.json());
@@ -39,13 +41,15 @@ const allowedOrigins = ['https://ebook-client-two.vercel.app'];
 
 app.use(cors({
   origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
+    if(!origin || allowedOrigins.includes(origin)){
+      callback(null, origin)
+    }else{
       callback(new Error('Not allowed by CORS'));
     }
   },
   credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'x-auth-token', 'x-access-token', 'x-refresh-token', 'x-csrf-token', 'x-csrf-token'],
 }));
 
 app.options('*', cors({
