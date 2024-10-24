c15df59d139acdca7b62841e68a1d20046d1a12d
unknown <facuugerez37@gmail.com>
2024-10-24 18:06:05 -0300

    bro no se aplico el push

--- src/app.ts
+++ src/app.ts
@@ -53,7 +53,7 @@ app.options('*', (req, res) => {
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.header('Access-Control-Allow-Credentials', 'true');
-  res.sendStatus(200);
+  res.sendStatus(200); // Respuesta con estado OK
 });
 
 // set security HTTP headers
