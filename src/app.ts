fa1410efaf89df99399fa880a8cfa6795f507907
unknown <facuugerez37@gmail.com>
2024-10-24 21:19:44 -0300

    borrado vercel.json y reconfigurado cors en app.ts

--- src/app.ts
+++ src/app.ts
@@ -13,10 +13,39 @@ import { authLimiter } from './middlewares/rateLimiter';
 
 const app = express();
 
+// cors  enabled
+const allowedOrigins = ['https://ebook-client-two.vercel.app'];
+app.use((req, res, next) => {
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Origin', req.headers.origin || 'https://ebook-client-two.vercel.app');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    return res.sendStatus(200); // Usar 200 en lugar de 204 puede resolver ciertos problemas de CORS.
+  }
+  next();
+});
+
+const corsOptions: cors.CorsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+app.use(cors(corsOptions));
+
 if (config.env !== 'test') {
   app.use(morgan.successHandler);
   app.use(morgan.errorHandler);
 }
+
 
 // parse json request body
 app.use(express.json());
@@ -30,25 +59,7 @@ app.use(xss());
 // gzip compression
 app.use(compression());
 
-// enable cors
-const allowedOrigins = ['https://ebook-client-two.vercel.app'];
-
-const corsOptions: cors.CorsOptions = {
-  origin: 'https://ebook-client-two.vercel.app',
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-};
-
-app.use(cors(corsOptions));
-
-app.options('*', (req, res) => {
-  res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.sendStatus(204); // Enviar una respuesta vacía con código 204 (sin contenido)
-});
+// passport initalize
 app.use(passport.initialize());
 
 // limit repeated failed requests to auth endpoints
