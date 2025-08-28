394cb71a2dce39c7088751cc0853bfd81affc86b
unknown <facuugerez37@gmail.com>
2025-08-28 18:52:55 -0300

    cors fix v2

--- src/app.ts
+++ src/app.ts
@@ -15,24 +15,7 @@ const app = express();
 
 // enable cors
 
-const allowedOrigins = [
-  'https://ebook-client.netlify.app',
-  'http://localhost:3000' 
-];
-
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('CORS not allowed'));
-    }
-  },
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
-
+app.use(cors());
 app.options('*', cors());
 
 //enviroment config
